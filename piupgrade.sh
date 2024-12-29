2311984a7edce0abeaf41ca554ddeb6a429f18e6
ubuntupunk <ubuntupunk@gmail.com>
2024-12-29 12:12:51 +0200

    added pi

---
+++ piupgrade.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo ""
+echo "Updating/Upgrading Raspbian"
+apt-get -y update
+apt-get -y upgrade
+apt-get -y dist-upgrade
+apt-get -y install rpi-update
+rpi-update
+echo "Updating/Upgrading completed"
+echo ""
